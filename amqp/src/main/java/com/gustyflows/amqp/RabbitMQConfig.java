2db596c5371e1bf2268788b4c381ec7d29b5e29d
augustas3000 <augusto.pasaulis@gmail.com>
2022-02-14 17:07:53 +0200

    rabbit mq image and basic amqp template configuration

---
+++ amqp/src/main/java/com/gustyflows/amqp/RabbitMQConfig.java
@@ -0,0 +1,32 @@
+package com.gustyflows.amqp;
+
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AllArgsConstructor
+public class RabbitMQConfig {
+
+    private final ConnectionFactory connectionFactory;
+
+    @Bean
+    public AmqpTemplate amqpTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jacksonConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public MessageConverter jacksonConverter() {
+//        Jackson2ObjectMapperBuilder.json().modules()....
+//        possible to supply a customized object mapper for more complex cases
+        return new Jackson2JsonMessageConverter();
+    }
+
+}
